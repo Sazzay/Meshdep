5104d38378275a7a285447c7fc67342d9b121b23
Markus Larsson <mark.larsson.93@gmail.com>
2020-03-08 17:17:59 +0100

    Initial http server

---
+++ includes/httpserver.py
@@ -0,0 +1,16 @@
+# https://docs.python.org/3/library/http.server.html
+
+class RequestHandler(SimpleHTTPRequestHandler):
+	def do_HEAD(self):
+		pass
+
+	def do_GET(self):
+		pass
+
+	def do_POST(self):
+		# login
+
+		# register
+
+		# file transfer up/down
+		pass
